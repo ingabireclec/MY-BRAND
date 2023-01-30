9fb6ecde4368097f4a29a97005e35a3b036ac01f
ingabireclec <ingabireclec@gmail.com>
2023-01-30 13:46:36 +0200

    dashboard bug fix

--- contactFormValid.js
+++ contactFormValid.js
@@ -47,4 +47,24 @@ Form.addEventListener("submit", function (event) {
   }
   if (Message === "") {
   }
+  // Get form elements
+  const nameInput = document.querySelector("#name");
+  const emailInput = document.querySelector("#email");
+  const messageInput = document.querySelector("#message");
+
+  // Create array to store all contact forms
+  let queries = JSON.parse(localStorage.getItem("queries")) || [];
+
+  // Create an object to store form input values
+  const contact = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  // Add form input values to contacts array
+  queries.push(contact);
+
+  // Save contacts array to localStorage
+  localStorage.setItem("queries", JSON.stringify(queries));
 });
